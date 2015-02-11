60fe237f0d3bb68f74401052bc3e59dc8429e9be
Breno Ramalho Lemes <breno@br-lemes.net>
2015-02-11 12:20:21 -0300

    Change package.cpath to load C modules from bin subdir.

--- www/index.lua
+++ www/index.lua
@@ -4,6 +4,15 @@ Content-type: text/html
 
 <html>
 <body>
-	Hello <%= _VERSION %>
+	<%
+		local root_path = ENV.DOCUMENT_ROOT:match("^(.+/).+/?$")
+		package.cpath = root_path .. "?.so;./?.so"
+		package.path = root_path .. "?.lua;./?.lua"
+
+		require("lfs")
+		require("luasql.sqlite3")
+	%>
+	Hello <%= _VERSION %><br>
+	<%= lfs.currentdir() %><br>
 </body>
 </html>
